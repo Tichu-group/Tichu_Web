403d103653675683a60fbc3ac3fcb2da5e0a8bfd
JungInRhee <ysrheee@kaist.ac.kr>
2022-02-01 01:13:54 +0900

    Update .eslint.js: allow require syntax

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   }
 };
